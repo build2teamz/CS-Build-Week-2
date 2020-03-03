7155a20514efc87c5cbae9be446001711e40ac4c
Jenn Soderborg <jsoderborg7@gmail.com>
2020-03-03 09:37:28 -0700

    basic traversal working

--- player.py
+++ player.py
@@ -1,14 +1,38 @@
+import requests
+import json
+import time
+
+auth_key = 'Token 4b31da83b4eea06cbdb3cef9a44f916ed593c634'
+
+headers = {
+    'Authorization': auth_key,
+    'Content-Type': 'application/json'
+}
+
 class Player:
-  def __init__(self, name, startingRoom):
-    self.name = name
-    self.currentRoom = startingRoom
-    self.cooldown = 1
-    self.encumbrance = 0
-    self.strength = 0
-    self.speed = 0
-    self.gold = 0
-    self.inventory = []
-    self.status = []
-    self.errors = []
-    self.messages = []
-    self.mine = ""
+    def __init__(self, name, startingRoom):
+        self.name = name
+        self.currentRoom = startingRoom
+    def travel(self, direction):
+        if direction == "n":
+            data={"direction": "n"}
+        if direction == "s":
+            data={"direction": "s"}
+        if direction == "e":
+            data={"direction": "e"}
+        if direction == "w":
+            data={"direction": "w"}
+        response=requests.post(
+            'https://lambda-treasure-hunt.herokuapp.com/api/adv/move/', headers=headers, data=json.dumps(data)
+        )
+
+        nextRoom=json.loads(response.text)
+        self.currentRoom=nextRoom
+
+    def init(self):
+        response=requests.get(
+            'https://lambda-treasure-hunt.herokuapp.com/api/adv/init/', headers=headers 
+        )
+        nextRoom=json.loads(response.text)
+        self.currentRoom=nextRoom
+        print(response.text)
---
+++ traversal.py
@@ -0,0 +1,99 @@
+from player import Player
+import time 
+
+player=Player('jenn', 0)
+player.init()
+
+
+
+traversalPath = []
+#-----------
+copy={}
+rooms={}
+reverse=[]
+#-----------
+while len(copy) < 500:
+  cooldown=player.currentRoom['cooldown']
+  time.sleep(cooldown)
+  time.sleep(2)
+
+  current_room=player.currentRoom['room_id']
+  if current_room not in copy:
+    copy[current_room]=current_room
+    exits={}
+  
+    for exit in player.currentRoom['exits']:
+      exits[exit]="unknown"
+
+    copy[current_room]=exits
+  
+  exits=copy[current_room]
+
+  if current_room not in rooms:
+      rooms[current_room]=current_room
+      roomObj=player.currentRoom
+      rooms[current_room]=roomObj
+      print(roomObj)
+      
+
+
+  if 'n' in copy[current_room] and exits['n'] == 'unknown':
+    if exits['n']=='unknown':
+      player.travel("n")
+      traversalPath.append("n")
+      newRoom=player.currentRoom['room_id']
+      exits['n']=newRoom
+      newExits={}
+      if newRoom not in copy:
+        for exit in player.currentRoom['exits']:
+          newExits[exit]="unknown"
+          copy[newRoom]=newExits
+        newExits['s']=current_room
+      reverse.append('s')
+
+  elif 's' in copy[current_room] and exits['s'] == 'unknown':
+    if exits['s']=='unknown':
+      player.travel("s")
+      traversalPath.append("s")
+      newRoom=player.currentRoom['room_id']
+      exits['s']=newRoom
+      newExits={}
+      if newRoom not in copy:
+        for exit in player.currentRoom['exits']:
+          newExits[exit]="unknown"
+          copy[newRoom]=newExits
+        newExits['n']=current_room
+      reverse.append('n')
+
+  elif 'e' in copy[current_room] and exits['e'] == 'unknown':
+    if exits['e']=='unknown':
+      player.travel("e")
+      traversalPath.append("e")
+      newRoom=player.currentRoom['room_id']
+      exits['e']=newRoom
+      newExits={}
+      if newRoom not in copy:
+        for exit in player.currentRoom['exits']:
+          newExits[exit]="unknown"
+          copy[newRoom]=newExits
+        newExits['w']=current_room
+      reverse.append('w')
+
+  elif 'w' in copy[current_room] and exits['w'] == 'unknown':
+    if exits['w']=='unknown':
+      player.travel("w")
+      traversalPath.append("w")
+      newRoom=player.currentRoom['room_id']
+      exits['w']=newRoom
+      newExits={}
+      if newRoom not in copy:
+        for exit in player.currentRoom['exits']:
+          newExits[exit]="unknown"
+          copy[newRoom]=newExits
+        newExits['e']=current_room
+      reverse.append('e')
+
+  else: 
+    reversal=reverse.pop()
+    player.travel(reversal)
+    traversalPath.append(reversal)
